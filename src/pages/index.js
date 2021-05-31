872e7619f4b522ebebaa1eeb4a2d1e2f3be48fa0
pympk <yee_ping@yahoo.com>
2021-05-31 14:51:48 -0700

    youtube time 5880s

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,25 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { page, text } from "../examples/home.module.css"
+import { ExampleButton } from "../examples/button"
 
 export default function Home() {
   return (
     <Layout>
-      <div className="home-heading">
-        <h1>++ Home ++</h1>
+      {/* <div className="home-heading"> */}
+      <div className={page}>
+        <ExampleButton>click me</ExampleButton>
+        <h1>++ home ++</h1>
         <h1>hello from home page</h1>
+        <p className={text}>
+          Adipisicing mollit exercitation eu sint dolore. Ex veniam tempor enim
+          eiusmod deserunt veniam officia dolor aliqua laborum. Excepteur
+          eiusmod veniam occaecat adipisicing dolor anim eu. Consequat Lorem
+          adipisicing elit eiusmod fugiat.
+        </p>
         <div>
-          <Link to="/about">About gatsby link</Link>
+          <Link to="/about">about gatsby link</Link>
         </div>
         <div>
           <Link to="/company/history">company history</Link>
@@ -17,9 +27,9 @@ export default function Home() {
         <div>
           <a href="https://www.gatsbyjs.com/docs/">hef link</a>
         </div>
-        <div>
+        {/* <div>
           <Link to="https://www.gatsbyjs.com/docs/">gatsby Link</Link>
-        </div>
+        </div> */}
       </div>
     </Layout>
   )
