98e3a37ded1c4919ef00847d4b6253857985e32c
pympk <yee_ping@yahoo.com>
2021-06-12 16:13:55 -0700

    youtube 27660s

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { BsClockHistory, BsClock, BsPeople } from "react-icons/bs"
 import Layout from "../components/Layout"
@@ -20,7 +20,49 @@ const RecipeTemplate = ({ data }) => {
     <Layout>
       <main className="page">
         <div className="recipe-page">
-          <h2>{title}</h2>
+          {/* hero */}
+          <section className="recipe-hero">
+            <GatsbyImage
+              image={pathToImage}
+              alt={title}
+              className="about-img"
+            />
+            <article className="recipe-info">
+              <h2>{title}</h2>
+              <p>{description}</p>
+              {/* icons */}
+              <div className="recipe-icons">
+                <article>
+                  <BsClock />
+                  <h5>prep time</h5>
+                  <p>{prepTime} min</p>
+                </article>
+                <article>
+                  <BsClockHistory />
+                  <h5>cook time</h5>
+                  <p>{cookTime} min</p>
+                </article>
+                <article>
+                  <BsPeople />
+                  <h5>serving</h5>
+                  <p>{servings}</p>
+                </article>
+              </div>
+              {/* tags */}
+              <p className="recipe-tags">
+                Tags:
+                {tags.map((tag, index) => {
+                  return (
+                    <Link to={`/${tag}`} key={index}>
+                      {tag}
+                    </Link>
+                  )
+                })}
+              </p>
+            </article>
+          </section>
+          {/* rest of the content */}
+          <section className="recipe-content"></section>
         </div>
       </main>
     </Layout>
